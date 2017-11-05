9546f13d98849e0689a8f66dce781bc524e3f797
jaredfiorali <jaredfiorali@gmail.com>
2017-11-05 08:07:01 -0500

    Created a framework for the configuration file

---
+++ includes/config/config_sample.php
@@ -0,0 +1,28 @@
+<?php
+
+class Config {
+
+	const db_host = '';
+
+	const db_username = '';
+
+	const db_password = '';
+
+	const db_database = '';
+
+	const darksky_api_key = '';
+
+	const weather_latitude = '';
+
+	const weather_longitude = '';
+
+	const pocket_access_tokens = ['', ''];
+
+	const pocket_consumer_key = '';
+
+	public function __constructor() {
+		// Silence is golden
+	}
+}
+
+?>
