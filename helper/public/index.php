4fb56beb0b0d04a3dc6ee56199c3c3894f4190ba
Jared Fiorali <11296421+jaredfiorali@users.noreply.github.com>
2020-07-26 16:54:10 -0400

    Removing some abstract work from the photoframe helper

--- helper/public/index.php
+++ helper/public/index.php
@@ -32,28 +32,18 @@ $app->get(
 		// Loop...forever
 		while (true) {
 
-			error_log('Reading weather information from the database');
-
 			// Get the weather from the database
 			$result = $app['db']->fetchOne("CALL getWeather()");
 
 			// Store the result from the database in a variable
 			$weather_result = $result['weather'] ?? null;
 
-			// Confirm that something has changed from what we knew before
-			if ($weather != $weather_result) {
-
-				error_log('----------Weather information has updated, posting to client via SSE----------');
-
-				// Update the saved weather result with the new one
-				$weather = $weather_result;
-
-				// Echo it so the client will pick up the latest data
-				echo $weather_result;
-			}
+			// Echo it so the client will pick up the latest data
+			echo $weather_result;
 
 			// Flush the output buffer
 			flush();
+			ob_flush();
 
 			// Wait 10 seconds, and then do it all again
 			sleep(10);
@@ -63,7 +53,7 @@ $app->get(
 
 // Sets up a lister for the weather service
 $app->get(
-	'/listenTest',
+	'/index',
 	function () use ($app) {
 
 		// SSE needs to be set to 'text/event-stream', and since we are accepting connections from photoframe.fiora.li...CORS
