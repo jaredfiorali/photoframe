57b809cac82291667332c6e1a0aa4d63683815dd
Jared Fiorali <jaredfiorali@gmail.com>
2020-10-03 22:52:21 +0000

    Turning on development mode

--- frame/app/services/ConfigService.php
+++ frame/app/services/ConfigService.php
@@ -15,7 +15,7 @@ class ConfigService extends Injectable {
 	 * If we should have certain development features enabled (minification for instance)
 	 * @var boolean DEVELOPMENT_MODE
 	**/
-	public $development_mode = false;
+	public $development_mode = true;
 
 	/**
 	 * The database host
