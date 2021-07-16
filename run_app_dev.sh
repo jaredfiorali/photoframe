979ba2c0d8657c84be17f8f71b410d9e6d6b837d
Jared Fiorali <jaredfiorali@gmail.com>
2021-07-16 14:20:19 -0400

    Fix up volume mounts for dev work

--- run_app_dev.sh
+++ run_app_dev.sh
@@ -1,7 +1,18 @@
 # /bin/bash
 
-# Build Docker image
-docker build app -t react-dev
+# Check to see if we are planning on building a new image
+if [ ! -z "$1" ]; then
+	echo "Building Docker image"
+
+	# Build Docker image
+	docker build app -t react-dev
+else
+	echo "Skipping Docker image build"
+fi
 
 # Run Docker image
-docker run -p 3000:3000 react-dev
+docker run \
+-p 3000:3000 \
+--mount type=bind,source=`pwd`/app/files/src,target=/app/src \
+--mount type=bind,source=`pwd`/app/files/public,target=/app/public \
+react-dev:latest
